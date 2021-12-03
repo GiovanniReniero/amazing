5ee024c7a10063c99cb75d57fe74bea0eb8f3b49
Giovanni Reniero <giovanni.reniero@gmail.com>
2021-12-03 18:25:31 +0000

    Working on algorithm

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ const walls = [
 World.add(world, walls ) //add shape to the World Object
 
 
+const shuffle =(arr) => {
+  let counter = arr.length;
+  n=0
+   while (n < counter){
+     let index = Math.floor(Math.random()*counter)
+     let temp = arr[index]
+     arr[index]= arr[n]
+     arr[n] = temp
+     n++
+   }
+  return arr
+}
+ 
 const grid = Array(cells).fill([]).map(() => Array(cells).fill(false));
 const verticals = Array(cells).fill([]).map(()=>Array(cells-1).fill(false));
 const horizontals = Array(cells-1).fill([]).map(()=>Array(cells).fill(false));
@@ -48,9 +61,35 @@ const firstIndex  = Math.floor(Math.random()*(cells));
 const secondIndex  = Math.floor(Math.random()*(cells));
 
 let startCell = grid[firstIndex][secondIndex]
-console.log (startCell)
-console.log (firstIndex, secondIndex) 
+// console.log (firstIndex, secondIndex) 
 
-const stepThroughCell = (cells) => {
+const stepThroughCell = (row, column) => {
+  //if I have visited a cell at [row, column], then return
+  if (grid[row][column]){
+    return
+  }
+  //mark the cell as being visited
+  grid[row][column]=true
+  //assemble randomly assembled-ordered list of neighbours
+  neighbours = shuffle([
+    [row-1, column],
+    [row, column+1],
+    [row, column-1],
+    [row+1, column],
+  ]);
+  console.log(neighbours)
+  //for each neighbour ....
   
+  // see if the neighbour is out or bounds
+  
+  //if we have visited that neighbour, continue to next neighbour
+  
+  //Remove a wall from either horizontals or verticals
+  
+  //visit the next cell
+
 }
+
+// stepThroughCell(firstIndex, secondIndex)
+stepThroughCell(1, 1)
+// console.log (grid)
